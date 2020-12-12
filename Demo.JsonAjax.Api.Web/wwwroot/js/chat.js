39e18693aec9d98963cf9286734a8128a24be20e
David Urting <david.urting@live.com>
2020-12-12 23:56:28 +0100

    async/await gebruikt ipv Promise.

--- Demo.JsonAjax.Api.Web/wwwroot/js/chat.js
+++ Demo.JsonAjax.Api.Web/wwwroot/js/chat.js
@@ -1,49 +1,52 @@
 ﻿// Event handler voor submit button.
-document.querySelector('input[type="submit"]').addEventListener("click", function (e) {
-    // bericht POST'en
-    fetch("/api/chat",
-        {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                nickname: document.querySelector('input[name="nickname"]').value,
-                message: document.querySelector('input[name="message"]').value
-            })
-        })
-        .then(() => {
-            document.querySelector('input[name="message"]').value = "";
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-
+document.querySelector('input[type="submit"]').addEventListener("click", async (e) => {
     // Dit zorgt ervoor dat de form niet gepost wordt (= dus geen refresh)!
     e.preventDefault(); 
+
+    try {
+        // bericht POST'en
+        await fetch("/api/chat",
+            {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    nickname: document.querySelector('input[name="nickname"]').value,
+                    message: document.querySelector('input[name="message"]').value
+                })
+            });
+        document.querySelector('input[name="message"]').value = "";
+    } catch (ex) {
+        console.error(error);
+    }
 });
 
 // Berichten om de seconde ophalen
-function getMessages() {
-    fetch("/api/chat")
-        .then((response) => { // Omzetten van de JSON body naar een JS object
-            return response.json();
-        })
-        .then((chatMessages) => { // Dan kan het JS object gebruikt worden
-            let chatHistory = "";
-            for (let i = 0; i < chatMessages.length; i++) {
-                chatHistory +=
-                    chatMessages[i].nickname + ": " + chatMessages[i].message + '\n';
-            }
-            document.querySelector("textarea").value = chatHistory;           
-        })
-        .catch((error) => { // Als er iets misloopt in de ‘ketting’
-            console.error(error);
-        });
+async function getMessages() {
+    try {
+        let response = await fetch("/api/chat");
+        let chatMessages = await response.json(); // Omzetten van de JSON body naar een JS object
+
+        // Dan kan het JS object gebruikt worden
+        let chatHistory = "";
+        for (let i = 0; i < chatMessages.length; i++) {
+            chatHistory +=
+                chatMessages[i].nickname + ": " + chatMessages[i].message + '\n';
+        }
+        document.querySelector("textarea").value = chatHistory;
+    } catch (ex) {
+        console.error(error);
+    }
 }
 
-// Een eerste maal de messages ophalen
-getMessages();
+// Een eerste maal de messages ophalen...
+// Aangezien await een async function vereist werd onderstaande constructie gebruikt.
+// (Onderstaande constructie noemt men ook wel een IIFE.) 
+// Meer info: https://stackoverflow.com/questions/46515764/how-can-i-use-async-await-at-the-top-level
+(async () => {
+    await getMessages();
+})();
 
-// Elke seconden de messages opnieuw ophalen.
+// En vervolgens elke seconde de messages opnieuw ophalen.
 setInterval(getMessages, 1000);
